91ecbc764a42bb78b5f3345b23cc25a80b5ed89b
adnanmushtaq1996 <adnanmushtaq5@gmail.com>
2022-11-20 01:29:20 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Driver_Sleep_Detection
 A Computer Vision based Project to check if the driver of a vehicle is sleepy
+
+# About the Project
+
+The main idea of this project is to protect drivers who travel a long distance, especially during the night time, and are prone to feel sleepy.
+
+The project can be deployed on a Raspberry Pi and can be installed along with a camera on the dashboard of the car. The system will automatically check if the person is sleepy and sound an alarm when the person seems to be sleepy.
+
+The project is inspired by [here](https://pyimagesearch.com/2017/05/08/drowsiness-detection-opencv/).
+
+# Working of the Project
+
+1. The first step is to detect if a face can be identified using the [dlib](http://dlib.net/) library.
+2. Once a face is found, the coordinates of the eye are selected. The coordinates of the face are shown in the [image](https://www.researchgate.net/publication/343699139_A_framework_for_facial_age_progression_and_regression_using_exemplar_face_templates) below. 
+    
+    ![face coordinates](Images/face_cordinates.jpg)
+3. Once the eyes are identified, the Eye Aspect Ratio (EAR) for each eye is calculated using the equation shown below.
+   
+   ![EAR](Images/EAR.png)
+
+    <img src="https://latex.codecogs.com/svg.image?\inline&space;\huge&space;\bg{black}EAR&space;=&space;\frac{(P2-P6)&space;&plus;&space;(P3-P5)}{2(P1-P4)}" title="https://latex.codecogs.com/svg.image?\inline \huge \bg{black}EAR = \frac{(P2-P6) + (P3-P5)}{2(P1-P4)}" />
+
+4. The average EAR of both eyes is taken.
+5. If the Average EAR of both eyes falls below a threshold for a given consecutive number of frames, then the alarm is sounded and the driven can be woken.
+
+# Prerequisites for Windows
+
+1. Install [CMake](https://cmake.org/download/): Since dlib is C-based, CMake is required for the build. Also, add CMake to your Environment Variables.
+2. Install Visual Studio [here](https://visualstudio.microsoft.com/visual-cpp-build-tools/): Also install additional package Packages `CMake tools for Windows`
+
+
+# Run the Code
+
+1. Download the dblib models for face detection [here](http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2).
+2. Install requirements.txt using pip `install -r requirements.txt`
+
+3. In order to test or run the code, run the command `python main.py`
+   
+# Additional Information
+
+## Python Version
+
+The whole project is developed with python version `Python 3.7.7` and pip version `pip 19.2.3`.
+
+## Contact
+In case of an error, feel free to contact me over Linkedin at [Adnan](https://www.linkedin.com/in/adnan-karol-aa1666179/).
+
+
---
+++ main.py
@@ -0,0 +1,116 @@
+"""
+Created on Sat Nov 19 23:21:57 2022
+@author = Karol
+"""
+
+# Import Dependencies
+
+import json
+import cv2
+from scipy.spatial import distance
+import dlib
+from imutils import face_utils
+import imutils
+
+# Define the Path to the Config File
+path_to_config_file = "config.json"
+
+"""
+Function to load config parameters
+"""
+def config_params():
+
+    with open(path_to_config_file, 'r') as f:
+        config = json.load(f)
+    
+    EAR_threshold = config["EAR_threshold"]
+    number_of_frames = config["number_of_frames"]
+
+    return EAR_threshold, number_of_frames
+
+
+"""
+Function to Calculate the EAR of an eye
+"""
+def eye_aspect_ratio(eye):
+
+	# Euclidean distance between the two sets of vertical coordinates
+	EAR1 = distance.euclidean(eye[1], eye[5])
+	EAR2 = distance.euclidean(eye[2], eye[4])
+
+	# Euclidean distance between the two sets of horizontal coordinates
+	EAR3 = distance.euclidean(eye[0], eye[3])
+
+	EAR = (EAR1 + EAR2) / (2.0 * EAR3)
+
+	return EAR
+
+
+"""
+Driver Function
+"""
+def driver_sleep_detector():
+
+    EAR_threshold, number_of_frames = config_params()
+
+    # dlib based face detection
+    detect = dlib.get_frontal_face_detector()
+    predict = dlib.shape_predictor("dlib_models\shape_predictor_68_face_landmarks.dat")
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+
+    # Using my seconday camera, hence 1. If you want to use primary camera enter 0
+    cap = cv2.VideoCapture(1)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    counter = 0
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame. Closing the System ...")
+            break
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        persons = detect(gray, 0)
+        for person in persons:
+            shape = predict(gray, person)
+            shape = face_utils.shape_to_np(shape)
+
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            EAR = (leftEAR + rightEAR) / 2.0
+
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (255, 0, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (255, 0, 0), 1)
+
+            if EAR < EAR_threshold:
+                counter = counter + 1
+                print(counter)
+                if counter >= number_of_frames:
+                    cv2.putText(frame, "****************ALERT!****************", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    print("ALERT !!!")
+            else:
+                counter = 0
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    # When everything done, close the video
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    driver_sleep_detector()
